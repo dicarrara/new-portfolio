34d63790b3b3bffd5f23d5a92a324187fb2a0aba
Diana <dianavcarrara@gmail.com>
2019-05-12 21:02:50 -0600

    added portfolio

---
+++ js/custom.js
@@ -0,0 +1,90 @@
+! function($) {
+    "use strict";
+
+    var myPortfolio = function() {};
+
+    //make overflow visible
+    myPortfolio.prototype.showOverflow = function() {
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    },
+
+    //scroll, add if it more than 50 px from the top
+    myPortfolio.prototype.stickyNav = function() {
+        $(window).on('scroll',function() {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 50) {
+                $(".sticky").addClass("stickyadd");
+            } else {
+                $(".sticky").removeClass("stickyadd");
+            }
+        });
+    },
+
+    //make it scroll smooth
+    myPortfolio.prototype.smoothScroll = function() {
+        $('.navbar-nav a, .scroll_down a').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    },
+
+    //ScrollSpy
+    myPortfolio.prototype.navSpyScroll = function() {
+        $("#navbarCollapse").scrollspy({
+            offset: 20
+        });
+    },
+
+    // //Portfolio Filter
+    // myPortfolio.prototype.filterForPortfolio = function() {
+    //     $(window).on('load', function () {
+    //         var $container = $('.work-filter');
+    //         var $filter = $('#menu-filter');
+    //         $container.isotope({
+    //             filter: '*',
+    //             layoutMode: 'masonry',
+    //             animationOptions: {
+    //                 duration: 750,
+    //                 easing: 'linear'
+    //             }
+    //         });
+
+   
+
+   
+
+    //Typed Text
+    myPortfolio.prototype.initTypedText = function() {
+        $(".element").each(function() {
+            var $this = $(this);
+            $this.typed({
+                strings: $this.attr('data-elements').split(','),
+                typeSpeed: 100,
+                backDelay: 3000
+            });
+        });
+    },  
+
+    myPortfolio.prototype.init = function() {
+        this.showOverflow();
+        this.stickyNav();
+        this.smoothScroll();
+        this.navSpyScroll();
+        this.initTypedText();
+    },
+
+    //init
+    $.myPortfolio = new myPortfolio, $.myPortfolio.Constructor = myPortfolio
+}(window.jQuery),
+
+//initializing
+function($) {
+    "use strict";
+    $.myPortfolio.init();
+}(window.jQuery);
