954ce91ef58182322bce6e7976b6bf018a3d7521
Diana <dianavcarrara@gmail.com>
2019-05-13 21:02:31 -0600

    added css

--- js/custom.js
+++ js/custom.js
@@ -1,47 +1,51 @@
-! function($) {
-    "use strict";
+!(function($) {
+  "use strict";
 
-    var myPortfolio = function() {};
+  var myPortfolio = function() {};
 
-    //make overflow visible
-    myPortfolio.prototype.showOverflow = function() {
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
-    },
-
+  //make overflow visible
+  (myPortfolio.prototype.showOverflow = function() {
+    $("body")
+      .delay(350)
+      .css({
+        overflow: "visible"
+      });
+  }),
     //scroll, add if it more than 50 px from the top
-    myPortfolio.prototype.stickyNav = function() {
-        $(window).on('scroll',function() {
-            var scroll = $(window).scrollTop();
+    (myPortfolio.prototype.stickyNav = function() {
+      $(window).on("scroll", function() {
+        var scroll = $(window).scrollTop();
 
-            if (scroll >= 50) {
-                $(".sticky").addClass("stickyadd");
-            } else {
-                $(".sticky").removeClass("stickyadd");
-            }
-        });
-    },
-
+        if (scroll >= 50) {
+          $(".sticky").addClass("stickyadd");
+        } else {
+          $(".sticky").removeClass("stickyadd");
+        }
+      });
+    }),
     //make it scroll smooth
-    myPortfolio.prototype.smoothScroll = function() {
-        $('.navbar-nav a, .scroll_down a').on('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 0
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    },
-
+    (myPortfolio.prototype.smoothScroll = function() {
+      $(".navbar-nav a, .scroll_down a").on("click", function(event) {
+        var $anchor = $(this);
+        $("html, body")
+          .stop()
+          .animate(
+            {
+              scrollTop: $($anchor.attr("href")).offset().top - 0
+            },
+            1500,
+            "easeInOutExpo"
+          );
+        event.preventDefault();
+      });
+    }),
     //ScrollSpy
-    myPortfolio.prototype.navSpyScroll = function() {
-        $("#navbarCollapse").scrollspy({
-            offset: 20
-        });
-    },
-
-    // //Portfolio Filter
+    (myPortfolio.prototype.navSpyScroll = function() {
+      $("#navbarCollapse").scrollspy({
+        offset: 20
+      });
+    }),
+    // //Portfolio Filter - check out isotrope.js
     // myPortfolio.prototype.filterForPortfolio = function() {
     //     $(window).on('load', function () {
     //         var $container = $('.work-filter');
@@ -55,36 +59,30 @@
     //             }
     //         });
 
-   
-
-   
-
     //Typed Text
-    myPortfolio.prototype.initTypedText = function() {
-        $(".element").each(function() {
-            var $this = $(this);
-            $this.typed({
-                strings: $this.attr('data-elements').split(','),
-                typeSpeed: 100,
-                backDelay: 3000
-            });
+    (myPortfolio.prototype.initTypedText = function() {
+      $(".element").each(function() {
+        var $this = $(this);
+        $this.typed({
+          strings: $this.attr("data-elements").split(","),
+          typeSpeed: 100,
+          backDelay: 3000
         });
-    },  
-
-    myPortfolio.prototype.init = function() {
-        this.showOverflow();
-        this.stickyNav();
-        this.smoothScroll();
-        this.navSpyScroll();
-        this.initTypedText();
-    },
-
+      });
+    }),
+    (myPortfolio.prototype.init = function() {
+      this.showOverflow();
+      this.stickyNav();
+      this.smoothScroll();
+      this.navSpyScroll();
+      this.initTypedText();
+    }),
     //init
-    $.myPortfolio = new myPortfolio, $.myPortfolio.Constructor = myPortfolio
-}(window.jQuery),
-
-//initializing
-function($) {
+    ($.myPortfolio = new myPortfolio()),
+    ($.myPortfolio.Constructor = myPortfolio);
+})(window.jQuery),
+  //initializing
+  (function($) {
     "use strict";
     $.myPortfolio.init();
-}(window.jQuery);
+  })(window.jQuery);
